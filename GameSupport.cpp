a5853305d43d04e2a7def4e318faa085c62592a8
Evan Mason <ecm025@gmail.com>
2024-03-02 17:22:04 -0500

    house cleaning

---
+++ GameSupport.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Evan Mason on 2/24/24.
+//
+
+#include "GameSupport.h"
---
+++ GameSupport.h
@@ -0,0 +1,8 @@
+//
+// Created by Evan Mason on 2/24/24.
+//
+
+#ifndef HELLOSFML_GAMESUPPORT_H
+#define HELLOSFML_GAMESUPPORT_H
+
+#endif //HELLOSFML_GAMESUPPORT_H
